212f964985e3c15580f365ff42e99d8a41674847
James Hughes <jhughes1010@gmail.com>
2021-10-30 19:23:52 -0600

    interrupt routines created

--- I2C_pulse_counter.ino
+++ I2C_pulse_counter.ino
@@ -22,9 +22,9 @@
 #define I2C_SLAVE_ADDRESS 0x30
 
 volatile uint8_t command = 0;
-volatile uint16_t windCount = 0x3c74;
-volatile uint16_t rainCount = 0xaa55;
-volatile bool LEDState = LOW;
+volatile uint16_t windCount = 0x0000;
+volatile uint16_t rainCount = 0x0000;
+volatile bool LEDState = HIGH;  //reverse logic signal
 
 //===============================================
 // setup
@@ -35,19 +35,19 @@ void setup()
   TinyWireS.onReceive(receiveEvent);
   TinyWireS.onRequest(requestEvent);
 
-  /*
-    //Interrupt pin for counter setup
-    cli();
-    PCMSK |= (1 << PCINT4) | (1 << PCINT5);   // Enable interrupt handler (ISR) for counter 0 and counter 1
-    GIMSK |= (1 << PCIE);                     // Enable PCINT interrupt in the general interrupt mask
-    pinMode(WIND_PIN, INPUT_PULLUP);
-    pinMode(RAIN_PIN, INPUT_PULLUP);
-    sei();
-  */
+
+  //Interrupt pin for counter setup
+  cli();
+  PCMSK |= (1 << PCINT4) | (1 << PCINT5);   // Enable interrupt handler (ISR) for counter 0 and counter 1
+  GIMSK |= (1 << PCIE);                     // Enable PCINT interrupt in the general interrupt mask
+  pinMode(WIND_PIN, INPUT_PULLUP);
+  pinMode(RAIN_PIN, INPUT_PULLUP);
+  sei();
+
 
   // Turn on LED when program starts
   pinMode(LED_PIN, OUTPUT);
-  //jh digitalWrite(LED_PIN, LEDState);
+  digitalWrite(LED_PIN, LEDState);
 }
 
 //===============================================
@@ -58,58 +58,11 @@ void loop()
   // This needs to be here
   TinyWireS_stop_check();
   digitalWrite(LED_PIN, LEDState);
-  delay(1000);
-  LEDState = !LEDState;
+  //delay(1000);
+  //LEDState = !LEDState;
 }
 
-//===============================================
-// I2C Write command
-//===============================================
-void receiveEvent(void)
-{
-  command = 0;
-  command = TinyWireS.read();
 
-  switch (command)
-  {
-    case RESET_WINDCOUNT:
-      windCount = 0;
-      break;
-
-    case RESET_RAINCOUNT:
-      rainCount = 0;
-      break;
-
-    case RESET_MINWINDPERIOD:
-      break;
-
-    case LED_TOGGLE:
-      LEDState = !LEDState;
-      break;
-  }
-}
-
-//===============================================
-// I2C Read command
-//===============================================
-void requestEvent(void)
-{
-  switch (command)
-  {
-    case RD_WINDCOUNT:
-      TinyWireS.write(windCount >> 8 & 0xFF);
-      TinyWireS.write(windCount & 0xFF);
-      break;
-
-    case RD_RAINCOUNT:
-      TinyWireS.write(rainCount >> 8 & 0xFF);
-      TinyWireS.write(rainCount & 0xFF);
-      break;
-
-    default:
-      blinkLED(5);
-  }
-}
 
 //===============================================
 // Blink LED
@@ -133,8 +86,21 @@ ISR(PCINT_VECTOR)
 {
   //determine interrupt pin mask that fired ISR
 
-  //software debounce TBD
-  int current = millis();
+  long int currentTime = millis();
+  static long int lastTime = 0;
   //or possible simple delay to see if we can keep busy
-  delay(10);
+  //delay(10);
+  if ((currentTime - lastTime) > 50)
+  {
+    if (!digitalRead(WIND_PIN))
+    {
+      windCount++;
+    }
+    if (!digitalRead(RAIN_PIN))
+    {
+      rainCount++;
+    }
+    blinkLED(1);
+  }
+  lastTime = currentTime;
 }
---
+++ i2c.ino
@@ -0,0 +1,48 @@
+//===============================================
+// I2C Write command
+//===============================================
+void receiveEvent(void)
+{
+  command = 0;
+  command = TinyWireS.read();
+
+  switch (command)
+  {
+    case RESET_WINDCOUNT:
+      windCount = 0;
+      break;
+
+    case RESET_RAINCOUNT:
+      rainCount = 0;
+      break;
+
+    case RESET_MINWINDPERIOD:
+      break;
+
+    case LED_TOGGLE:
+      LEDState = !LEDState;
+      break;
+  }
+}
+
+//===============================================
+// I2C Read command
+//===============================================
+void requestEvent(void)
+{
+  switch (command)
+  {
+    case RD_WINDCOUNT:
+      TinyWireS.write(windCount >> 8 & 0xFF);
+      TinyWireS.write(windCount & 0xFF);
+      break;
+
+    case RD_RAINCOUNT:
+      TinyWireS.write(rainCount >> 8 & 0xFF);
+      TinyWireS.write(rainCount & 0xFF);
+      break;
+
+    default:
+      blinkLED(5);
+  }
+}
